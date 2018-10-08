acaf60c92681e7090768fe978e994507e77aee32
CarelS072 <carelstarreveld@hotmail.com>
2018-10-08 17:26:48 +1300

    js-calc html + css lightly done

---
+++ scripts/js-calculator.js
@@ -0,0 +1,18 @@
+
+document.addEventListener('DOMContentLoaded', startCalc)
+
+
+// function displayResult() {
+//   document.getElementById("one").innerHTML = "2";
+// }
+
+function startCalc(){
+var total = 0;
+document.getElementById("display").innerHTML = total;
+
+
+
+document.addEventListener("click", displayResult);
+document.getElementById("one").innerHTML = "1";
+
+}
