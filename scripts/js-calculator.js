a47e2b6e1457f6f85abe7ae1d3f06f41333b3668
CarelS072 <carelstarreveld@hotmail.com>
2018-10-09 17:31:12 +1300

    calculations working, only 1 decimal can be added, bug when finished calc and go direct into new calculation

--- scripts/js-calculator.js
+++ scripts/js-calculator.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', startCalc)
 var total = 0;
 var val1 = 0;
 var val2 = 0;
-var operatorValue= 0;
+var operatorValue= "";
 var displayChar = "";
 
 
@@ -16,8 +16,8 @@ function startCalc(){
   
 };
 
-function display (total) {
-  document.getElementById("display").innerHTML=total;
+function display (displayChar) {
+  document.getElementById("display").innerHTML=displayChar;
 };
 
 function clear (){
@@ -57,28 +57,46 @@ function clear (){
 
 
 function modulus (operator) {
-  total = val1;
 
   switch (operator) {
     
       case add:
-      display(total) = val1 + val2
+      display(val1 + val2)
+      console.log(operatorValue +" performed")
+      val1=parseInt(displayChar)
+      displayChar=""
+      val2=x;
         break;
       case subtract:
-      display(total)  = val1 - val2
+      display(val1 - val2)
+      console.log(operatorValue +" performed")
+      val1=parseInt(displayChar)
+      displayChar=""
+
+      val2=x;
         break;  
       case multiply:
-      display(total)  = val1 * val2
+      display(val1 * val2)
+      console.log(operatorValue +" performed")
+      val1=parseInt(displayChar)
+      displayChar=""
+
+      val2=x;
         break;
       case divide:
-      display(total)  = val1 / val2
+      display(val1 / val2) 
+      console.log(operatorValue +" performed")
+      val1=parseInt(displayChar)
+      displayChar=""
+
+      val2=x;
         break;
           
       default:
-        display('blank')
+      console.log("modulus broke")
     
   
-  };
+  }
  
 
 
@@ -86,17 +104,13 @@ function modulus (operator) {
 
 function pressedButton(){
 
-  console.log(buttons)
-//  var buttonsCount = buttons.length;
-// console.log(buttonsCount)
-
 var buttons = document.getElementsByTagName("button");
-  
+//console.log(buttons)
+
   for (var i = 0; i <= buttons.length; i += 1) {
-      buttons[i].onclick = function(e) {
-          
+      buttons[i].onclick = function() {
           console.log(this.id);
-          if (this.id == "one"){
+          if (this.id == "one"){  
             displayChar = displayChar + "1";
             display(displayChar)
 
@@ -104,50 +118,75 @@ var buttons = document.getElementsByTagName("button");
             displayChar = displayChar + "2";
             display(displayChar)
           } else if (this.id == "three"){
-            display(3)
+            displayChar = displayChar + "3";
+            display(displayChar)
           } else if (this.id == "four"){
-            display(4)
+            displayChar = displayChar + "4";
+            display(displayChar)
           } else if (this.id == "five"){
-            display(5)
+            displayChar = displayChar + "5";
+            display(displayChar)
           } else if (this.id == "six"){
-            display(6)
+            displayChar = displayChar + "6";
+            display(displayChar)
           } else if (this.id == "seven"){
-            display(7)
+            displayChar = displayChar + "7";
+            display(displayChar)
           } else if (this.id == "eight"){
-            display(8)
+            displayChar = displayChar + "8";
+            display(displayChar)
           } else if (this.id == "nine"){
-            display(9)
+            displayChar = displayChar + "9";
+            display(displayChar)
           } else if (this.id == "zero"){
-            display(0)
+            displayChar = displayChar + "0";
+            display(displayChar)
           } else if (this.id == "add"){
-            val1=parseInt(displayChar) 
-            operatorValue = add;
-            display('+')
+            if (val2===x){
+              val2=parseInt(displayChar) 
+              displayChar="";
+              operatorValue = add;
+            } else{
+              val1=parseInt(displayChar) 
+              displayChar="";
+              operatorValue = add;
+            }
+        
           } else if (this.id == "subtract"){
             val1=parseInt(displayChar) 
+            displayChar="";
             operatorValue = subtract;
-            display('-')
           } else if (this.id == "divide"){
             val1=parseInt(displayChar) 
+            displayChar="";
             operatorValue = divide;
-            display('/')
           } else if (this.id == "multiply"){
-            val1=parseInt(displayChar) 
+            val1=parseInt(displayChar)
+            displayChar="";
             operatorValue = multiply;
-            display('*')
           } else if (this.id == "decimal"){
-            displayChar = displayChar + ".";
-            display(displayChar)
-            //display('.')
+            if (displayChar.lastIndexOf(".")==-1){
+              displayChar = displayChar + ".";
+                console.log(displayChar.lastIndexOf("."))
+              display(displayChar)
+            } else {
+                console.log(displayChar.lastIndexOf("."))
+              display(displayChar)
+            }
           } else if (this.id == "clear"){
             clear()
-          }
+          } else if (this.id == "equals"){
+             val2 =parseInt(displayChar)
+             modulus(operatorValue)
+            // display(displayChar)
+          } 
+ 
 
 
 
 
 
 
-      }
+     }
   }
 };
